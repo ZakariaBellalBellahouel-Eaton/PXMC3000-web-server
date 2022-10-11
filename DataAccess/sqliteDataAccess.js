1342fafa168b3efd98b5f0f428c0c848bc999bbd
Zakaria Bellal Bellahouel <ZakariaBellalBellahouel@eaton.com>
2022-10-11 10:06:26 -0400

    first commit

---
+++ DataAccess/sqliteDataAccess.js
@@ -0,0 +1,26 @@
+
+// Sqlite data access function
+const sqlite3 = require('sqlite3').verbose();
+const databasePath = "C:/Eaton/Software development/Code/Databasefile/SMPConfiguration.db";
+const getDeviInformationSqlQuery = "Select * from SmpDevice;";
+
+
+// Get device information data from Sqlite Database
+function getDeviceInformation(callback) {
+    const configurationDatabase = new sqlite3.Database(databasePath, sqlite3.OPEN_READWRITE, (err) => {
+        if (err) return callback(err.message)
+    });
+
+    configurationDatabase.all(getDeviInformationSqlQuery, [], (err, rows) => {
+        callback(err, rows)
+    });
+
+    configurationDatabase.close((err) => {
+        if (err)
+            callback("Error closing the database connection. Details : " + err.message);
+    });
+}
+
+
+// Export modules
+module.exports = { getDeviceInformation };
---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const host = 'localhost';
+const port = 8000;
+const sqliteDataAccess = require("./DataAccess/sqliteDataAccess");
+
+const requestListener = function (request, response) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.writeHead(200, "Content-Type", "application/json");
+    console.log("Received request.")
+    switch (request.url) {
+        case "/":
+            response.end(`{ "status" : "success", "message":"the service is running"}`);
+        // Handling GET device infotmation request
+        case "/DeviceInformation":
+            sqliteDataAccess.getDeviceInformation(function (err, data) {
+                if (err)
+                    response.end(`{ "status" : "error", "message":"${err}"}`);
+                response.end(JSON.stringify(data));
+            });
+            break;
+    }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
