fc5f60c4ad471b06ced301e8a04351611c01d23e
Zakaria Bellal Bellahouel <ZakariaBellalBellahouel@eaton.com>
2022-10-31 10:55:49 -0400

    Update the webpack.config.js to get the list of external node module dynamically

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
-const path = require('path');
+var path = require('path');
+var fs = require('fs');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    filename: 'SmpWebServer.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  externals: {
-    sqlite3: 'sqlite3',
-  },
-  target: 'node',
-};
+var nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function (x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function (mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
+
+console.log(nodeModules); module.exports = {
+    entry: './src/index.js',
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'SmpWebServer.js'
+    },
+    externals: nodeModules
+}
